19b23a0c71d9ee89ff781cd7bc443a6155d49a29
ALI <ahmedaliou94@gmail.com>
2021-09-01 08:05:59 -0400

    hacker rank repo

---
+++ betweenTwoSets.js
@@ -0,0 +1,37 @@
+// https://www.hackerrank.com/challenges/between-two-sets/problem
+
+// pseudo 
+
+// 1. find the LCM of all the integers of array A.
+// 2. find the GCD of all the integers of array B.
+// 3. Count the number of multiples of LCM that evenly divides the GCD.
+
+
+// gcd = a*b/lcm(of a, b)
+
+
+// echo "# hacker-rank" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/aliou94/hacker-rank.git
+// git push -u origin main
+
+
+
+
+a = [2,6]
+
+b = [24, 36]
+
+
+function getTotalX(a,b) {
+    
+
+   
+
+
+}
+
+getTotalX(a,b)
